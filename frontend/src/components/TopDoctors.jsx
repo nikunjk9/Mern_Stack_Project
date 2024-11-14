8a2cd4c01cd2f860b158ab792113ddea993fa6f7
nikunjk9 <nikunjkumar6379@gmail.com>
2024-11-14 16:11:53 +0530

    Completed Home Page

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -32,7 +32,7 @@ const TopDoctors = () => {
                 </div>
             ))}
         </div>
-        <button onClick= {()=>{ navigate('/doctors'); scroll(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+        <button onClick= {()=>{ navigate('/doctors'); scroll(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
       
     </div>
   )
