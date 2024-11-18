ef71f1ced3a1787b0785293c8e695e9dd3fb8aba
nikunjk9 <nikunjkumar6379@gmail.com>
2024-11-18 23:34:18 +0530

    Fixed error

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className=' flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>Welcome to HealthEase, your trusted partner in managing your healthcare needs conveniently and efficiently. At HealthEase, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
-          <p>HealthEase is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+          <p>HealthEase is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, HealthEase is here to support you every step of the way.</p>
           <b className='text-gray-800 text-xl '>Our Vision</b>
           <p className='italic hover:not-italic'>"Our vision at HealthEase is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it."</p>
         </div>
